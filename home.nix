2d1cabfc664ebae81f1f03431eaca9f31c605b35
Deadbeef <ent3rm4n@gmail.com>
2022-10-20 22:45:09 +0800

    better integration

--- home.nix
+++ home.nix
@@ -6,6 +6,62 @@
   home.username = "beef";
   home.homeDirectory = "/home/beef";
 
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+  home.packages = with pkgs; [
+    neovim
+    iosevka
+    wget
+    rnix-lsp
+    jetbrains.idea-community
+    obs-studio
+    kdenlive
+    rubberband
+    mlt
+    rustup
+  ];
+
+  programs.vscode = {
+    enable = true;
+    extensions = with pkgs.vscode-extensions; [
+      jnoortheen.nix-ide
+      rust-lang.rust-analyzer
+      ms-vscode-remote.remote-ssh
+      zhuangtongfa.material-theme
+#      tonybaloney.vscode-pets
+    ];
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Deadbeef";
+    userEmail = "ent3rm4n@gmail.com";
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    enableSyntaxHighlighting = true;
+    autocd = true;
+    plugins = [
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.5.0";
+          sha256 = "0za4aiwwrlawnia4f29msk822rj9bgcygw6a8a6iikiwzjjz0g91";
+        };
+      }
+    ];
+  };
+
+  programs.starship = {
+    enable = true;
+  };
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
@@ -18,35 +74,4 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-  home.packages = [
-    pkgs.neovim
-  #	pkgs.vscode
-    pkgs.iosevka
-    pkgs.wget
-    pkgs.rnix-lsp
-    pkgs.jetbrains.idea-community
-    pkgs.obs-studio
-    pkgs.kdenlive
-    pkgs.rubberband
-    pkgs.mlt
-    pkgs.rustup
-  ];
-  home.sessionVariables = {
-    EDITOR = "nvim";
-  };
-  programs.vscode = {
-    enable = true;
-    extensions = with pkgs.vscode-extensions; [
-      jnoortheen.nix-ide
-      rust-lang.rust-analyzer
-      ms-vscode-remote.remote-ssh
-      zhuangtongfa.material-theme
-#      tonybaloney.vscode-pets
-    ];
-  };
-  programs.git = {
-    enable = true;
-    userName = "Deadbeef";
-    userEmail = "ent3rm4n@gmail.com";
-  };
 }
